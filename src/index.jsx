7533eb5b06fef95f0403662aefec092fe546e213
Matt Brennan <mattyb1000@gmail.com>
2016-09-05 23:06:47 +0100

    refactor into connect button

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,9 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 
-const serviceCache = {};
-
-async function connectService(serviceUUID) {
-	if(serviceCache[serviceUUID]) return serviceCache[serviceUUID];
-
-	const device = await navigator.bluetooth.requestDevice({
-		filters: [{services: [serviceUUID]}],
-		optionalServices: [serviceUUID]
-	});
+async function connectService(device) {
 	const server = await device.gatt.connect()
-	return serviceCache[serviceUUID] = await server.getPrimaryService(serviceUUID);
+	return server.getPrimaryService(serviceUUID);
 };
 
 async function getCharacteristicValue(service, characteristicUUID) {
@@ -21,21 +13,27 @@ async function getCharacteristicValue(service, characteristicUUID) {
 
 const steps = value => 0x100 * value.getUint8(2) + value.getUint8(1);
 
-async function getSteps() {
-	const service = await connectService(0xfee7);
-	const value = await getCharacteristicValue(service, 0xfea1);
+async function getSteps(device, serviceUUID) {
+	const value = await getCharacteristicValue(connectService(serviceUUID), 0xfea1);
 	return steps(value);
 }
 
-class App extends Component {
+class ConnectDevice extends Component {
 	state = {};
 
+	async connect() {
+		const device = await navigator.bluetooth.requestDevice({
+			filters: [{services: [this.props.serviceUUID]}],
+			optionalServices: [this.props.serviceUUID],
+		});
+
+		this.setState({device});
+		if(this.props.onconnect) this.props.onconnect(device);
+	}
+
 	render() {
-		return <main>
-			<button onClick={() => getSteps().then(steps => this.setState({steps}))}>Get steps</button>
-			{this.state.steps}
-		</main>;
+		return <button onClick={() => this.connect()} disabled={!!this.state.device}>{this.state.device ? `Connected to ${this.state.device.name}` : 'Connect'}</button>;
 	}
 }
 
-render(<App />, document.body);
+render(<ConnectDevice serviceUUID={0xfee7} />, document.body);
